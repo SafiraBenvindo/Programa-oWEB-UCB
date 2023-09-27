0309804c5dc628c3c7c7cbd7379e412ed0b8199c
Vitória Safira de Oliveira <80469017+SafiraBenvindo@users.noreply.github.com>
2023-09-27 10:36:08 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express= require('express');
+const mustacheExpress = require ('mustache-express');
+const app = express ();
+
+
+
+app.engine('html', mustacheExpress());
+app.set('view engine','html');
+app.set('views', __dirname + '/views');
+
+app.get('/', function(req,res){
+    let usuario= {
+        nome : "Safira",
+        telefone : 12345678
+    };
+   
+    res.render('index.html', {usuario});
+})
+
+const PORT = 8080;
+app.listen(PORT, function(){
+    console.log('app rodando na porta ' +PORT);
+
+});
